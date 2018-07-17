57d5c9bb6fb5388515c674689374eacffd786b86
Cathleen Melendez <cathleen@melendez.com>
2018-07-17 23:01:42 +0000

    Made character counter turn red if character number is exceeded

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
   $(".new-tweet textarea").on("input", function () {
     let charNumber = 140 - this.value.length;
     $(".counter").text(charNumber);
+      if(charNumber < 0) {
+        $(".counter").addClass("invalid");
+      } else {
+        $(".counter").removeClass("invalid")
+      }
     })
   });
 
