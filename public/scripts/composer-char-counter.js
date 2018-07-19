54ea9a78ee04454780641f954d244dd98f613bdb
Cathleen Melendez <cathleen@melendez.com>
2018-07-19 18:39:26 +0000

    Please enter the commit message for your changes. Lines starting  made no modifications

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  console.log("We are ready.");
+  console.log("char-counter is ready.");
   $(".new-tweet textarea").on("input", function () {
     let charNumber = 140 - this.value.length;
     $(".counter").text(charNumber);
       if(charNumber < 0) {
         $(".counter").addClass("invalid");
       } else {
-        $(".counter").removeClass("invalid")
+        $(".counter").removeClass("invalid");
       }
     })
   });
